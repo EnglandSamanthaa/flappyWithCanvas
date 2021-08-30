6f74323782c5ddab196c3f5a4fd2516d6b26ab20
Samantha England <64668742+EnglandSamanthaa@users.noreply.github.com>
2021-08-30 18:36:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ let angle = 0;
 let hue = 0;
 let frame = 0;
 let score = 0;
-let gameSpeed = 2;
+let gameSpeed = 3;
 
+//adds scrolling background
 const background = new Image();
 background.src = 'Images/desert_BG.png';
 
@@ -30,21 +31,27 @@ function handleBackground() {
     ctx.drawImage(background, BG.x2, BG.y, BG.width, BG.height);
 }
 
+
+//function that runs and draws the game. 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // ctx.fillRect(10, canvas.height - 90, 50, 50);
+    
     handleBackground();
     handleObstactles();
     handleParticles();
     bird.update();
     bird.draw();
+    
+    //draws score to top left corner
     ctx.fillStyle = 'orange';
     ctx.font = '90px Georgia';
     ctx.strokeText(score, 75, 70);
     ctx.fillText(score, 75, 70);
+
     handleCollisions();
     if (handleCollisions()) return;
     requestAnimationFrame(animate);
+
     angle+= 0.11;
     hue++;
     frame++;
@@ -52,24 +59,37 @@ function animate() {
 
 animate();
 
+// doesn't work, do more research on how to make touchscreen eventListeners for mobile. 
+function startTouch(ev) {
+    spacePressed = true;
+}
 
-window.addEventListener('keydown', function(e){
-    if (e.code === "Space" || e.touchstart === touchstart) spacePressed = true;
-});
+function init() {
+    canvas.ontouchstart = startTouch;
+}
+
+// using spacebar to move bird up.
+ window.addEventListener('keydown', function(e){
+     if (e.code === "Space" || e.touchstart === touchstart) spacePressed = true;
+ });
+
+
+
 
 window.addEventListener('keyup', function(e){
     if (e.code === "Space") spacePressed = false;
     bird.frameX = 0;
 });
 
+//Collision image
 const bang = new Image();
 bang.src = 'Images/bang.png';
 
-
+//Function for collision dection
 function handleCollisions(){
     for (let i = 0; i < obstaclesArray.length; i++){
         if (bird.x < obstaclesArray[i].x + obstaclesArray[i].width && bird.x + bird.width > obstaclesArray[i].x && ((bird.y < 0 + obstaclesArray[i].top && bird.y + bird.height > 0) || (bird.y > canvas.height - obstaclesArray[i].bottom && bird.y + bird.height < canvas.height ))){
-            //collision dectected
+            //when collision is dectected
             ctx.drawImage(bang, bird.x, bird.y, 50, 50);
             ctx.font = '30px Georgia';
             ctx.fillStyle = 'yellow';
