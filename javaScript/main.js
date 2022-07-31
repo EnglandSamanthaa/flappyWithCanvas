9f8ceceea5d7fda9432172732832022e443be266
Samantha England <64668742+EnglandSamanthaa@users.noreply.github.com>
2022-07-31 11:53:18 -0500

    Add files via upload

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,6 +1,10 @@
 // ----------------------- Variables
-const canvasHeight = innerHeight;
-const canvasWidth = innerWidth;
+var canvasHeight = innerHeight;
+var canvasWidth = innerWidth;
+
+const canvasContainer = document.getElementById('canvasContainer');
+canvasContainerHeight = canvasHeight;
+canvasContainerWidth = canvasWidth;
 
 //variables for the homescreen canvas
 const homeScreenCanvas = document.getElementById('startCanvas');
@@ -13,6 +17,11 @@ const gameScreenCanvas = document.getElementById('gameCanvas');
 const ctxGame = gameScreenCanvas.getContext('2d');
 gameScreenCanvas.height = canvasHeight;
 gameScreenCanvas.width = canvasWidth;
+
+const homeIcon = new Image();
+homeIcon.src = 'Images/homeIcon.png';
+homeIcon.height = 100;
+homeIcon.width = 100;
 
 //Image and variables for the start button
 const startButton = new Image();
@@ -45,117 +54,16 @@ const BG = {
 
 
 
-var char = '';
-var spacePressed = false;
-var angle = 0;
-var hue = 0;
-var frame = 0;
-var score = 0;
-var gameSpeed = 2;
-
-
-// ------------------------------------------------------    event listeners    -------------------------------------------------------------------
-
-// using spacebar to move bird up.
-window.addEventListener('keydown', function (e) {
-    if (e.code === "Space" || e.touchstart === touchstart) spacePressed = true;
-});
-
-window.addEventListener('keyup', function (e) {
-    if (e.code === "Space") spacePressed = false;
-    char.frameX = 0;
-});
-
-
-
-//listens for mouse clicks on the canvas
-homeScreenCanvas.addEventListener('click', (event) => {
-
-    const rect = homeScreenCanvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    //console.log("x: " + x + " y: " + y);
-    //console.log(event);
-
-    
-    //listens for a click on the green bird and selects green bird for character.
-    if (x >= greenBirdSelection.xpoint && x <= greenBirdSelection.xpoint + greenBirdSelection.width && y >= greenBirdSelection.ypoint && y <= greenBirdSelection.ypoint + greenBirdSelection.height) {
-        //only happens if the character isn't already green bird.
-        if(char != greenBirdCharacter){
-
-            //Selects green bird as the in game character
-            char = greenBirdCharacter;
-            console.log(char.name);
-
-            //makes the green bird larger, while returning other bird if previous selected to their orginal display size.
-            greenBirdSelection.ypoint = greenBirdSelection.ypoint - 5;
-            greenBirdSelection.height = greenSelectionHeight * 1.5;
-            greenBirdSelection.width = greenSelectionWidth * 1.5;
-            pinkBirdSelection.height = pinkSelectionHeight;
-            pinkBirdSelection.width = pinkSelectionWidth;
-            grumpyBirdSelection.height = grumpySelectionHeight;
-            grumpyBirdSelection.width = grumpySelectionWidth;   
-        };   
-    };
-
-    
-    //listens for a click on the pink bird and selects pink bird for character
-    if (x >= pinkBirdSelection.xpoint && x <= pinkBirdSelection.xpoint + 175 && y >= pinkBirdSelection.ypoint && y <= pinkBirdSelection.ypoint + 125) {
-        //only happens if pink bird isn't already selected
-        if(char != pinkBirdCharacter){
-
-            //Selects pink bird as the in game character
-            char = pinkBirdCharacter;
-            console.log(char.name);
-
-            //makes the pink bird larger, while returning other bird if previous selected to their orginal display size. 
-            greenBirdSelection.height = greenSelectionHeight;
-            greenBirdSelection.width = greenSelectionWidth;
-            pinkBirdSelection.ypoint = pinkBirdSelection.ypoint - 5;
-            pinkBirdSelection.height = pinkSelectionHeight * 1.5;
-            pinkBirdSelection.width = pinkSelectionWidth * 1.5;
-            grumpyBirdSelection.height = grumpySelectionHeight;
-            grumpyBirdSelection.width = grumpySelectionWidth;
-        };
-    };
-
-    
-    //listens for a click on the grumpy bird and selects grumpy bird for character
-    if (x >= grumpyBirdSelection.xpoint && x <= grumpyBirdSelection.xpoint + 175 && y >= grumpyBirdSelection.ypoint && y <= grumpyBirdSelection.ypoint + 125) {
-        //only happens if grumpy bird is not already selected
-        if (char != grumpyBirdCharacter){
-
-            //Changes the in game character to grumpy bird
-            char = grumpyBirdCharacter;
-            console.log(char.name)
-
-            //makes the grumpy bird larger, while returning other bird if previous selected to their orginal display size.
-            greenBirdSelection.height = greenSelectionHeight;
-            greenBirdSelection.width = greenSelectionWidth;
-            pinkBirdSelection.height = pinkSelectionHeight;
-            pinkBirdSelection.width = pinkSelectionWidth;
-            grumpyBirdSelection.ypoint = grumpyBirdSelection.ypoint - 5;
-            grumpyBirdSelection.height = grumpySelectionHeight* 1.5;
-            grumpyBirdSelection.width = grumpySelectionWidth*1.5;
-        };
-    };
-
-
-    //listens for start button to be pressed
-    if (x >= startButtonX + 20 && x <= startButtonX + startButtonWidth && y >= startButtonY + 24 && y <= startButtonY+ startButtonHeight - 22) {
-        // console.log('start button pressed');
-
-        //allows game screen to sit on top of home screen
-        homeScreenCanvas.style.zIndex = '-1';
-        gameScreenCanvas.style.zIndex = '0';
-
-        gamePlay();
-    };
-
-
-});
-
-
+let char = null;
+let spacePressed = false;
+let angle = 0;
+let hue = 0;
+let frame = 0;
+let score = 0;
+let gameSpeed = 2;
+let currentDisplay = 'home';
+let homeScreenAnimation;
+let gamePlayAnimation;
 
 
 // -----------------------------------------------------------   functions  -------------------------------------------------------------
@@ -170,8 +78,7 @@ function handleBackground() {
     else BG.x2 -= gameSpeed;
 
 
-    if (homeScreenCanvas.style.zIndex === '-1'){
-        
+    if (currentDisplay === 'game'){   
         //runs the scrolling background during game play.
         ctxGame.drawImage(backgroundLayer5, BG.x1, BG.y, BG.width, BG.height);
         ctxGame.drawImage(backgroundLayer5, BG.x2, BG.y, BG.width, BG.height);
@@ -205,7 +112,6 @@ function handleBackground() {
 
 //runs the homescreen, draws player selection and startbutton
 function homeScreen() {
-
     ctxStart.clearRect(0, 0, homeScreenCanvas.width, homeScreenCanvas.height);
 
     //function on main.js... deals with the scrolling background. 
@@ -224,14 +130,49 @@ function homeScreen() {
     ctxStart.drawImage(startButton, startButtonX, startButtonY, startButtonWidth, startButtonHeight);
 
     //creates the animation loop for the homescreen
-    requestAnimationFrame(homeScreen);
+    homeScreenAnimation = requestAnimationFrame(homeScreen);
 };
 
-//function is on main.js
-homeScreen();
+
+function handleCollisons(){
+
+    for(let i = 0; i < obstaclesArray.length; i++){
+        //determining if bird comes into contact with an obstacle
+        if (char.x <= obstaclesArray[i].x + obstaclesArray[i].width
+            //left side of obstacle
+            && char.x + char.width >= obstaclesArray[i].x
+            //top side of bottom obstatcles 
+            && ((char.y <= 0 + obstaclesArray[i].top     
+            && char.y + char.height > 0) 
+            //bottoms of top obstacles
+            || (char.y >= gameScreenCanvas.height - (obstaclesArray[i].bottom) 
+            && char.y + char.height < gameScreenCanvas.height))
+            ){
+               return true; 
+            };
+    };
+};
+
+function returnToHomeScreen(){
+    gameScreenCanvas.style.display = 'none';
+    homeScreenCanvas.style.display = 'block';
+    char = null;
+    spacePressed = false;
+    angle = 0;
+    hue = 0;
+    frame = 0;
+    score = 0;
+    currentDisplay = 'home';
+
+    ctxStart.clearRect(0, 0, homeScreenCanvas.width, homeScreenCanvas.height);
+    homeScreen();
+
+};
+
 
 
 function gamePlay(){
+
     ctxGame.clearRect(0, 0, gameScreenCanvas.width, gameScreenCanvas.height);
     
     //function on main.js... deals with the scrolling background. 
@@ -241,14 +182,173 @@ function gamePlay(){
     //function is on obstacle.js.... creates the pipes on the top and bottom of the screen
     handleObstactles();
 
+    ctxGame.font = '100px Black Ops One';
+    ctxGame.fillStyle = 'white';
+    ctxGame.fillText('Score : ' + score, (canvasWidth/4)*2.5, (canvasHeight/4) * 3.5, (canvasWidth/4), (canvasHeight/10));
+
     //function is on bird.js in character constructor... draws the character onto the gamescreen during game play
     char.draw();
 
     //function is on bird.js in character constructor... updates the birds position and animates the bird. 
     char.update();
 
+    handleCollisons();
+    if(handleCollisons()) {
+
+        gameScreenCanvas.onclick = function (e){
+            console.log('click listener, gameScreen');
+            const gameRect = gameScreenCanvas.getBoundingClientRect();
+            const gameX= e.clientX - gameRect.left;
+            const gameY = e.clientY - gameRect.top;
+            console.log("x: " + gameX + " y: " + gameY);
+            console.log(e);
+
+            if(gameX >= 100 && gameX <= 200 && gameY >= 100 && gameY <= 200){
+                homeScreenReturnAllCharacterOptionsToOrigninalSize();
+                returnToHomeScreen();
+            }
+        };
+
+        ctxGame.font = '200px Black Ops One';
+        ctxGame.fillStyle = 'white';
+        ctxGame.fillText('Game Over!', (canvasWidth/4)*.4, (canvasHeight/2)*1.2, (canvasWidth/2), (canvasHeight/3));
+
+        ctxGame.drawImage(homeIcon, 100, 100, 100, 100);
+        cancelAnimationFrame(gamePlayAnimation);
+
+        return;
+    };
+
     //creates the animation loop for the gamescreen
-    requestAnimationFrame(gamePlay);
+    gamePlayAnimation = requestAnimationFrame(gamePlay);
 
     frame++;
+};
+
+function homeScreenGreenBirdSelectedResize(){
+    greenBirdSelection.ypoint = greenBirdSelection.ypoint - 5;
+    greenBirdSelection.height = greenSelectionHeight * 1.5;
+    greenBirdSelection.width = greenSelectionWidth * 1.5;
+    pinkBirdSelection.height = pinkSelectionHeight;
+    pinkBirdSelection.width = pinkSelectionWidth;
+    grumpyBirdSelection.height = grumpySelectionHeight;
+    grumpyBirdSelection.width = grumpySelectionWidth;   
+};
+
+function homeScreenPinkBirdSelectedResize(){
+    greenBirdSelection.height = greenSelectionHeight;
+    greenBirdSelection.width = greenSelectionWidth;
+    pinkBirdSelection.ypoint = pinkBirdSelection.ypoint - 5;
+    pinkBirdSelection.height = pinkSelectionHeight * 1.5;
+    pinkBirdSelection.width = pinkSelectionWidth * 1.5;
+    grumpyBirdSelection.height = grumpySelectionHeight;
+    grumpyBirdSelection.width = grumpySelectionWidth;
+};
+
+function homeScreenGrumpyBirdSelectedResize(){
+    greenBirdSelection.height = greenSelectionHeight;
+    greenBirdSelection.width = greenSelectionWidth;
+    pinkBirdSelection.height = pinkSelectionHeight;
+    pinkBirdSelection.width = pinkSelectionWidth;
+    grumpyBirdSelection.height = grumpySelectionHeight * 1.5;
+    grumpyBirdSelection.width = grumpySelectionWidth * 1.5;
+    grumpyBirdSelection.ypoint = grumpyBirdSelection.ypoint - 5;
+};
+
+function homeScreenReturnAllCharacterOptionsToOrigninalSize(){
+    greenBirdSelection.height = greenSelectionHeight;
+    greenBirdSelection.width = greenSelectionWidth;
+    pinkBirdSelection.height = pinkSelectionHeight;
+    pinkBirdSelection.width = pinkSelectionWidth;
+    grumpyBirdSelection.height = grumpySelectionHeight;
+    grumpyBirdSelection.width = grumpySelectionWidth;
+}
+
+homeScreen();
+
+// ------------------------------------------------------    event listeners    -------------------------------------------------------------------
+
+// using spacebar to move bird up.
+window.addEventListener('keydown', function (e) {
+    if (e.code === "Space" || e.touchstart === touchstart) spacePressed = true;
+});
+
+window.addEventListener('keyup', function (e) {
+    if (e.code === "Space") spacePressed = false;
+    char.frameX = 0;
+});
+
+
+
+
+window.onload = (event) => {
+
+
+//listens for mouse clicks on the homeScreencanvas
+homeScreenCanvas.onclick = function(event) {
+    console.log('click listener');
+    const rect = homeScreenCanvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    console.log("x: " + x + " y: " + y);
+    console.log(event);
+
+    
+    //listens for a click on the green bird and selects green bird for character.
+    if (x >= greenBirdSelection.xpoint && x <= greenBirdSelection.xpoint + greenBirdSelection.width && y >= greenBirdSelection.ypoint && y <= greenBirdSelection.ypoint + greenBirdSelection.height) {
+        //only happens if the character isn't already green bird.
+        if(char != greenBirdCharacter){
+            char = greenBirdCharacter;
+            console.log(char.name);
+            homeScreenGreenBirdSelectedResize()
+        };   
+    };
+    
+    //listens for a click on the pink bird and selects pink bird for character
+    if (x >= pinkBirdSelection.xpoint && x <= pinkBirdSelection.xpoint + 175 && y >= pinkBirdSelection.ypoint && y <= pinkBirdSelection.ypoint + 125) {
+        //only happens if pink bird isn't already selected
+        if(char != pinkBirdCharacter){
+            char = pinkBirdCharacter;
+            console.log(char.name);
+            homeScreenPinkBirdSelectedResize();
+        };
+    };
+
+    //listens for a click on the grumpy bird and selects grumpy bird for character
+    if (x >= grumpyBirdSelection.xpoint && x <= grumpyBirdSelection.xpoint + 175 && y >= grumpyBirdSelection.ypoint && y <= grumpyBirdSelection.ypoint + 125) {
+        //only happens if grumpy bird is not already selected
+        if (char != grumpyBirdCharacter){
+            char = grumpyBirdCharacter;
+            console.log(char.name)
+            homeScreenGrumpyBirdSelectedResize();
+        };
+    };
+
+
+    //listens for start button to be pressed
+    if (x >= startButtonX + 20 && x <= startButtonX + startButtonWidth && y >= startButtonY + 24 && y <= startButtonY+ startButtonHeight - 22 && char != null) {
+        // console.log('start button pressed');
+
+        homeScreenCanvas.style.display ='none';
+        gameScreenCanvas.style.display = 'block';
+        currentDisplay = 'game'
+        obstaclesArray.length = 0;
+
+        //functions the runs the game location on main.js
+        gamePlay();
+        cancelAnimationFrame(homeScreenAnimation);
+        ctxGame.clearRect(0, 0, gameScreenCanvas.width, gameScreenCanvas.height);
+
+        greenBirdCharacter.x = 200;
+        greenBirdCharacter.y = 300;
+        pinkBirdCharacter.x = 200;
+        pinkBirdCharacter.y = 300;
+        grumpyBirdCharacter.x = 200
+        grumpyBirdCharacter.y = 300;
+
+        
+    };
+
+};
+
 };
--- javaScript/obstacles.js
+++ javaScript/obstacles.js
@@ -1,30 +1,37 @@
+//bottom pipe image
 const pipeBottom = new Image();
 pipeBottom.src = 'Images/pipe.png';
 
+//top pipe image
 const pipeTop = new Image();
 pipeTop.src = 'Images/pipetop.png';
 
 const obstaclesArray = [];
 
+//obstacles constructor
 class Obstacle {
+    
     constructor(){
-        this.top = (Math.random() * gameScreenCanvas.height/5) + 30;
+        this.top = (Math.random() * gameScreenCanvas.height/3) + 30;
         this.bottom = (Math.random() * gameScreenCanvas.height/3) + 30;
         this.x = gameScreenCanvas.width + 60;
-        this.width = 100;
+        this.width = 200;
         this.counted = false;
 
         pipeBottom.height = this.top;
         pipeBottom.width = this.width;
-    }
+    };
+
 
     draw(){
+
         let pipeBottomHeight = this.bottom;
         let pipeTopHeight = this.top;
 
         ctxGame.drawImage(pipeTop, this.x - 40, 0, this.width, this.top);
         ctxGame.drawImage(pipeBottom, this.x - 40, gameScreenCanvas.height - this.bottom, this.width, this.bottom);
-    }
+    };
+
 
     update(){
         this.x -= gameSpeed + 2;
@@ -33,20 +40,19 @@ class Obstacle {
             this.counted = true;
         }
         this.draw();
+    };
 
-    }
-}
+};
 
 
 
 function handleObstactles(){
-    if (frame % 100 === 0){
-        obstaclesArray.unshift(new Obstacle);
-    }
-    for (let i = 0; i < obstaclesArray.length; i++){
-        obstaclesArray[i].update();
-    }
-    if (obstaclesArray.length > 20){
-        obstaclesArray.pop(obstaclesArray[0]);
-    }
-}
+    //creates a new obstacle once the frame hits 175,,, will determine spacing between obstacles
+    if (frame % 175 === 0) obstaclesArray.unshift(new Obstacle);
+
+    //
+    for (let i = 0; i < obstaclesArray.length; i++) obstaclesArray[i].update();
+
+    //removes already passed obstacles from the obstacle array
+    if (obstaclesArray.length > 20) obstaclesArray.pop(obstaclesArray[0]);
+};
